fe036e98b126a5cab7d725d9e715b9886d017b67
gonza56d <gonza56d@gmail.com>
2025-08-22 17:50:25 -0300

    Integration working

--- flights/api/repositories.py
+++ flights/api/repositories.py
@@ -1,3 +1,7 @@
+from datetime import datetime
+from http import HTTPStatus
+import requests
+
 from dataclasses import dataclass
 
 from flights.core.models import FlightEvent
@@ -12,4 +16,16 @@ class JourneysHTTPRepository(JourneysRepository):
     endpoint: str
 
     def get_flight_events(self) -> list[FlightEvent]:
-        raise Exception('This is working')
+        response = requests.get(url=f'{self.provider_base_url}{self.endpoint}')
+        if response.status_code != HTTPStatus.OK:
+            pass
+        return [
+            FlightEvent(
+                flight_number=result['flight_number'],
+                from_=result['departure_city'],
+                to=result['arrival_city'],
+                departure_time=datetime.fromisoformat(result['departure_datetime'].replace('Z', '+00:00')),
+                arrival_time=datetime.fromisoformat(result['arrival_datetime'].replace('Z', '+00:00')),
+            )
+            for result in response.json()
+        ]
--- flights/api/views.py
+++ flights/api/views.py
@@ -1,9 +1,11 @@
+from dataclasses import asdict
 from datetime import date
 
 from dependency_injector.wiring import inject, Provide
 from fastapi import APIRouter, Depends
 
-from flights.api.models import SearchJourneysRequest, SearchJourneysResponse
+from flights.api.models import FlightEvent, SearchJourneysRequest, SearchJourneysResponse
+from flights.core.models import Journey
 from flights.containers import FlightsContainer, FlightsCommandBus
 
 router = APIRouter(
@@ -19,9 +21,37 @@ async def search_journeys(
         destination: str,
         command_bus: FlightsCommandBus = Depends(Provide[FlightsContainer.command_bus]),
 ):
+    """
+    Search journeys available for given date, with the right origin and destinations.
+
+    Args:
+        date (date): The desired date of departure.
+        origin (str): 3-letter code indicating city of departure.
+        destination (str): 3-letter code indicating city of destination.
+
+    Returns:
+        list[SearchJourneysResponse]: n possible journeys, from which each of these
+        can have 1 or 2 flight events connected.
+    """
     action = SearchJourneysRequest(
         from_=origin,
         to=destination,
         date=date,
     ).get_action()
-    return command_bus.handle(action)
+    results: list[Journey] = command_bus.handle(action)
+    return [
+        SearchJourneysResponse(
+            connections=result.connections,
+            path=[FlightEvent(
+                **{
+                    'flight_number': flight_event.flight_number,
+                    'from': flight_event.from_,
+                    'to': flight_event.to,
+                    'departure_time': flight_event.departure_time,
+                    'arrival_time': flight_event.arrival_time,
+
+                }
+            ) for flight_event in result.flight_events],
+        )
+        for result in results
+    ]
