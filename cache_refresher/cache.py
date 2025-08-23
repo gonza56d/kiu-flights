4136d57cafd11df3973602543bc5d9eb94cbca8e
gonza56d <gonza56d@gmail.com>
2025-08-22 21:26:32 -0300

    Test commit 2

--- cache_refresher/cache.py
+++ cache_refresher/cache.py
@@ -19,4 +19,3 @@ class RedisCacheRepository(CacheRepository):
             self._cache_key,
             json.dumps([asdict(flight_event) for flight_event in results], default=str),
         )
-#test commit
